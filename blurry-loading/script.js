dfb44d45b0587d86477f64374c3eeb94312e9993
Virshree <virshreedesai19@gmail.com>
2024-11-24 14:04:35 +0530

    files added

---
+++ blurry-loading/script.js
@@ -0,0 +1,34 @@
+// let img = document.getElementById("image")
+ let text = document.querySelector(".text")
+
+// const keyframes = [
+//     {filter:"blur(5px)"},
+//     {filter:"blur(4px)"},
+//     {filter:"blur(3px)"},
+//     {filter:"blur(2px)"},
+//     {filter:"blur(0px)"}
+// ]
+
+// const options ={
+
+//     duration:5000,iterations:Infinity
+// }
+
+// const keyframes = []
+
+// function appendObjects(){
+//     for(let i=0;i<=100;i++)
+//     {
+//         keyframes.push({content:`${i}%`,opacity:`0.${i}`})
+        
+//     }
+    
+// }
+// appendObjects()
+// console.log(keyframes)
+// text.animate(keyframes,options)
+
+
+
+//  img.animate(keyframes,options)
+// console.log(text)
